8e0d496e7a5b82a049ac9ef0cf39997a48dce61d
Vasiliy <visekDI@yandex.ru>
2023-07-24 10:58:21 +0000

    rew2

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,5 +1,7 @@
+
 import os
 from pathlib import Path
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,9 +10,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = False
+DEBUG = os.getenv('DEBUG','')
 
-ALLOWED_HOSTS = ['84.201.142.105', '127.0.0.1', 'localhost', 'reviewdickies57.ddns.net']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -1,13 +1,10 @@
-from rest_framework import routers
-
-from django.contrib import admin
-from django.urls import include, path
-
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework import routers
 
 from cats.views import AchievementViewSet, CatViewSet
-
 
 router = routers.DefaultRouter()
 router.register(r'cats', CatViewSet)
